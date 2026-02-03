42dbbb00a3616abeb739531a28c6a5141284b944
thiago <thiagotsdelima@outlook.com>
2026-02-03 08:57:58 -0300

    adjust tailwind

--- src/App.vue
+++ src/App.vue
@@ -153,33 +153,33 @@ const filteredProducts = computed(() => {
       </div>
     </div>
 
-    <table class="w-full border-collapse">
+    <table class="w-full border-collapse sm:text-lg text-sm">
       <thead>
         <tr>
           <th
             v-for="col in columnsOrder"
             :key="col"
-            class="border border-gray-300 p-2"
+            class="border border-gray-300 p-2 hidden sm:table-cell"
           >
             {{ col }}
           </th>
-          <th class="border border-gray-300 p-2">Ações</th>
+          <th class="border border-gray-300 p-2 hidden sm:table-cell">Ações</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="(p, idx) in filteredProducts" :key="p.id || idx">
-          <td class="border border-gray-300 p-2 max-w-1">
-            <input v-model="p.id" type="number" min="0" max="5" />
+          <td class="border border-gray-300 sm:p-2 sm:max-w-1 p-1 max-w-6">
+            <input v-model="p.id" type="number" min="0" max="0" step="0" />
           </td>
           <td class="border border-gray-300 p-2">
-            <input v-model="p.name" class="w-full rounded p-1" />
+            <input v-model="p.name" class="sm:w-full rounded sm:p-1 w-16 p-0" />
           </td>
-          <td class="border border-gray-300 p-2 max-w-2">
+          <td class="border border-gray-300 sm:p-2 sm:max-w-2 p-1 max-w-14">
             <input
               v-model.number="p.price"
               type="number"
               min="0"
-              max="15"
+              max="100"
               step="0.1"
             />
           </td>
@@ -189,18 +189,18 @@ const filteredProducts = computed(() => {
           <td class="border border-gray-300 p-2">
             <input v-model="p.imageUrl" class="w-full rounded p-1" />
           </td>
-          <td class="border border-gray-300 p-1 w-6">
+          <td class="border border-gray-300 sm:px-6 sm:max-w-1 p-1 max-w-8">
             <input
               v-model.number="p.rating"
               type="number"
               min="0"
-              max="5"
-              step="0.1"
+              max="10"
+              step="0"
             />
           </td>
           <td class="border border-gray-300 p-2 text-center">
             <button
-              class="text-red-500 cursor-pointer hover:text-red-600"
+              class="text-red-500 cursor-pointer hover:text-red-600 text-xs sm:text-lg"
               @click="removeProduct(idx)"
             >
               Delete
